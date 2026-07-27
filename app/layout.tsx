5ba03ffeea4a9eb8c8e98f105724281fc48718b2
Allan <90080194+emeraldcr@users.noreply.github.com>
2026-07-27 11:19:12 -0600

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
           />
         </noscript>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18016737230"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18350075343"
           strategy="afterInteractive"
         />
         <Script id="google-ads-tag" strategy="afterInteractive">
